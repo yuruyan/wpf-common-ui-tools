77056c8b07b4faea441d414ff3140deaed4ec8cc
Luxiu123 <klt5466@163.com>
2022-06-14 20:56:06 +0800

    feat: 添加 NetworkUtilsTests 测试

---
+++ CommonUIToolsTests/Utils/NetworkUtilsTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CommonUITools.Utils.Tests {
+    [TestClass()]
+    public class NetworkUtilsTests {
+        [TestMethod()]
+        public void GetLocalIpAddress() {
+            Console.WriteLine($"local ip addresss is {NetworkUtils.GetLocalIpAddress()}");
+        }
+
+        [TestMethod()]
+        public void GetInUsePorts() {
+            Console.WriteLine("port in use:");
+            foreach (var port in NetworkUtils.GetInUsePorts()) {
+                Console.WriteLine(port);
+            }
+        }
+    }
+}
