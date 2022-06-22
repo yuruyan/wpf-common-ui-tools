16721caea063e09a74dbe4a11f6623622ec30023
Luxiu123 <klt5466@163.com>
2022-06-22 22:17:34 +0800

    mod: 修改 ConstantUtils

--- CommonUITools/Utils/ConstantUtils.cs
+++ CommonUITools/Utils/ConstantUtils.cs
@@ -1,22 +1,17 @@
 ﻿namespace CommonUITools.Utils;
 
 public static class ConstantUtils {
-    // 用户根目录 id
-    public static readonly byte USER_ROOT_DIR_ID = 0;
-    // 文件 id 长度
-    public static readonly byte FILE_ID_LENGTH = 32;
-    // 最大文件名长度
-    public static readonly byte MAX_FILENAME_LENGTH = 255;
-    // 1小时的秒数
-    public static readonly long ONE_HOUR_SECOND = 3600;
-    // 1小时的毫秒数
-    public static readonly long ONE_HOUR_MILLI = 3600000;
-    // 1天的秒数
-    public static readonly long ONE_DAY_SECOND = 86400;
-    // 1天的毫秒数
-    public static readonly long ONE_DAY_MILLI = 86400000;
-    // 1 MB 大小，以 byte 为单位
-    public static readonly long ONE_MB_SIZE = 1024 * 1024;
-    // 1 GB 大小，以 byte 为单位
-    public static readonly long ONE_GB_SIZE = 1024 * 1024 * 1024;
+    public const byte UserRootDirId = 0; // 用户根目录 id
+    public const byte FileIdLength = 32; // 文件 id 长度
+    public const byte MaxFilenameLength = 255; // 最大文件名长度
+    public const long OneMbSize = 1024 * 1024; // 1 MB 大小，以 byte 为单位
+    public const long OneGbSize = 1024 * 1024 * 1024; // 1 GB 大小，以 byte 为单位
+    public const int HalfHourSecond = 1800; // 半小时的秒数
+    public const int HalfHourMillisecond = 1800000; // 半小时的毫秒数
+    public const int OneHourSecond = 3600; // 1小时的秒数
+    public const int OneHourMillisecond = 3600000; // 1小时的毫秒数
+    public const int OneDaySecond = 86400; // 1天的秒数
+    public const int OneDayMillisecond = 86400000; // 1天的毫秒数
+    public const int SqlExecutionFailed = -1; // sql 执行失败
+    public const int ParameterInvalid = -1; // 参数错误
 }
