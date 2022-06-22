a40cdfef5242a9e2a3b1694b24e71faa17352a95
Luxiu123 <klt5466@163.com>
2022-06-22 15:45:00 +0800

    feat: 添加 CryptoUtils 重载方法

--- CommonUITools/Utils/CryptoUtils.cs
+++ CommonUITools/Utils/CryptoUtils.cs
@@ -11,8 +11,8 @@ public class CryptoUtils {
     /// 加密
     /// </summary>
     /// <param name="message"></param>
-    /// <param name="key"></param>
-    /// <param name="iv"></param>
+    /// <param name="key">32位16进制字符串</param>
+    /// <param name="iv">32位16进制字符串</param>
     /// <returns></returns>
     public static byte[] AESEncode(byte[] message, string key, string iv) {
         KeyParameter keyParam = ParameterUtilities.CreateKeyParameter("AES", Hex.Decode(key));
@@ -25,22 +25,30 @@ public class CryptoUtils {
     /// 加密
     /// </summary>
     /// <param name="message">使用 UTF8 编码</param>
+    /// <param name="key">32位16进制字符串</param>
+    /// <param name="iv">32位16进制字符串</param>
+    /// <returns></returns>
+    public static byte[] AESEncode(string message, string key, string iv) {
+        return AESEncode(Encoding.UTF8.GetBytes(message), key, iv);
+    }
+
+    /// <summary>
+    /// 加密后编码为 base64 字符串
+    /// </summary>
+    /// <param name="message"></param>
     /// <param name="key"></param>
     /// <param name="iv"></param>
     /// <returns></returns>
-    public static byte[] AESEncode(string message, string key, string iv) {
-        KeyParameter keyParam = ParameterUtilities.CreateKeyParameter("AES", Hex.Decode(key));
-        IBufferedCipher inCipher = CipherUtilities.GetCipher("AES/CBC/PKCS7Padding");
-        inCipher.Init(true, new ParametersWithIV(keyParam, Hex.Decode(iv)));
-        return inCipher.DoFinal(Encoding.UTF8.GetBytes(message));
+    public static string AESEncodeToBase64(string message, string key, string iv) {
+        return Convert.ToBase64String(AESEncode(Encoding.UTF8.GetBytes(message), key, iv));
     }
 
     /// <summary>
     /// 解密
     /// </summary>
     /// <param name="message"></param>
-    /// <param name="key"></param>
-    /// <param name="iv"></param>
+    /// <param name="key">32位16进制字符串</param>
+    /// <param name="iv">32位16进制字符串</param>
     /// <returns></returns>
     public static byte[] AESDecode(byte[] message, string key, string iv) {
         KeyParameter keyParam = ParameterUtilities.CreateKeyParameter("AES", Hex.Decode(key));
@@ -48,5 +56,15 @@ public class CryptoUtils {
         inCipher.Init(false, new ParametersWithIV(keyParam, Hex.Decode(iv)));
         return inCipher.DoFinal(message);
     }
-}
 
+    /// <summary>
+    /// 解密 base64 编码的数据
+    /// </summary>
+    /// <param name="data"></param>
+    /// <param name="key">32位16进制字符串</param>
+    /// <param name="iv">32位16进制字符串</param>
+    /// <returns></returns>
+    public static byte[] AESDecodeFromBase64(string data, string key, string iv) {
+        return AESDecode(Convert.FromBase64String(data), key, iv);
+    }
+}
