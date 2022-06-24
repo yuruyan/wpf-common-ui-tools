d4d6ee975ab1ee74998ea4b52b81113e9b57943d
Luxiu123 <klt5466@163.com>
2022-06-24 20:15:14 +0800

    mod: 添加 MapperConverters

--- CommonUITools/Converter/MapperConverter.cs
+++ CommonUITools/Converter/MapperConverter.cs
@@ -19,12 +19,20 @@ public static class MapperUtils {
     public static void AddCommonConverters(IMapperConfigurationExpression cfg) {
         cfg.CreateMap<long, DateTime>().ConvertUsing(new LongToDateTimeTypeConverter());
         cfg.CreateMap<DateTime, long>().ConvertUsing(new DateTimeToLongTypeConverter());
+        cfg.CreateMap<string, byte>().ConvertUsing(new StringToByteTypeConverter());
+        cfg.CreateMap<byte, string>().ConvertUsing(new ByteToStringTypeConverter());
+        cfg.CreateMap<string, sbyte>().ConvertUsing(new StringToSbyteTypeConverter());
+        cfg.CreateMap<sbyte, string>().ConvertUsing(new SbyteToStringTypeConverter());
+        cfg.CreateMap<string, short>().ConvertUsing(new StringToShortTypeConverter());
+        cfg.CreateMap<short, string>().ConvertUsing(new ShortToStringTypeConverter());
+        cfg.CreateMap<string, ushort>().ConvertUsing(new StringToUshortTypeConverter());
+        cfg.CreateMap<ushort, string>().ConvertUsing(new UshortToStringTypeConverter());
         cfg.CreateMap<string, int>().ConvertUsing(new StringToIntTypeConverter());
         cfg.CreateMap<int, string>().ConvertUsing(new IntToStringTypeConverter());
-        cfg.CreateMap<string, long>().ConvertUsing(new StringToLongTypeConverter());
-        cfg.CreateMap<long, string>().ConvertUsing(new LongToStringTypeConverter());
         cfg.CreateMap<string, uint>().ConvertUsing(new StringToUintTypeConverter());
         cfg.CreateMap<uint, string>().ConvertUsing(new UintToStringTypeConverter());
+        cfg.CreateMap<string, long>().ConvertUsing(new StringToLongTypeConverter());
+        cfg.CreateMap<long, string>().ConvertUsing(new LongToStringTypeConverter());
         cfg.CreateMap<string, ulong>().ConvertUsing(new StringToUlongTypeConverter());
         cfg.CreateMap<ulong, string>().ConvertUsing(new UlongToStringTypeConverter());
     }
@@ -45,6 +53,54 @@ public class LongToDateTimeTypeConverter : ITypeConverter<long, DateTime> {
 public class DateTimeToLongTypeConverter : ITypeConverter<DateTime, long> {
     public long Convert(DateTime source, long destination, ResolutionContext context) {
         return CommonUtils.ConvertToTimestamp(source);
+    }
+}
+
+public class StringToByteTypeConverter : ITypeConverter<string, byte> {
+    public byte Convert(string source, byte destination, ResolutionContext context) {
+        return System.Convert.ToByte(source);
+    }
+}
+
+public class ByteToStringTypeConverter : ITypeConverter<byte, string> {
+    public string Convert(byte source, string destination, ResolutionContext context) {
+        return source.ToString();
+    }
+}
+
+public class StringToSbyteTypeConverter : ITypeConverter<string, sbyte> {
+    public sbyte Convert(string source, sbyte destination, ResolutionContext context) {
+        return System.Convert.ToSByte(source);
+    }
+}
+
+public class SbyteToStringTypeConverter : ITypeConverter<sbyte, string> {
+    public string Convert(sbyte source, string destination, ResolutionContext context) {
+        return source.ToString();
+    }
+}
+
+public class StringToShortTypeConverter : ITypeConverter<string, short> {
+    public short Convert(string source, short destination, ResolutionContext context) {
+        return System.Convert.ToInt16(source);
+    }
+}
+
+public class ShortToStringTypeConverter : ITypeConverter<short, string> {
+    public string Convert(short source, string destination, ResolutionContext context) {
+        return source.ToString();
+    }
+}
+
+public class StringToUshortTypeConverter : ITypeConverter<string, ushort> {
+    public ushort Convert(string source, ushort destination, ResolutionContext context) {
+        return System.Convert.ToUInt16(source);
+    }
+}
+
+public class UshortToStringTypeConverter : ITypeConverter<ushort, string> {
+    public string Convert(ushort source, string destination, ResolutionContext context) {
+        return source.ToString();
     }
 }
 
