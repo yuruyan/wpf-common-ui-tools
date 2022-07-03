0634083b2eb696dd0a81a276321af8f4d2a0ba10
Luxiu123 <klt5466@163.com>
2022-07-03 10:28:31 +0800

    feat: 添加 FileNameIconWidget

---
+++ CommonUITools/Widget/FileNameIconWidget.xaml.cs
@@ -0,0 +1,70 @@
+﻿using CommonUITools.Converter;
+using NLog;
+using SharpVectors.Converters;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace CommonUITools.Widget;
+
+/// <summary>
+/// 文件名、图标 widget
+/// </summary>
+public partial class FileNameIconWidget : UserControl {
+    private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+    public static readonly DependencyProperty FileNameProperty = DependencyProperty.Register("FileName", typeof(string), typeof(FileNameIconWidget), new PropertyMetadata(string.Empty));
+    public static readonly DependencyProperty IconPathProperty = DependencyProperty.Register("IconPath", typeof(string), typeof(FileNameIconWidget), new PropertyMetadata(string.Empty));
+    public static readonly DependencyProperty IconSizeProperty = DependencyProperty.Register("IconSize", typeof(double), typeof(FileNameIconWidget), new PropertyMetadata(20.0));
+    public static readonly DependencyProperty AutoIconProperty = DependencyProperty.Register("AutoIcon", typeof(bool), typeof(FileNameIconWidget), new PropertyMetadata(true));
+
+    public string FileName {
+        get { return (string)GetValue(FileNameProperty); }
+        set { SetValue(FileNameProperty, value); }
+    }
+    /// <summary>
+    /// 图标，用于显示
+    /// </summary>
+    public string IconPath {
+        get { return (string)GetValue(IconPathProperty); }
+        set { SetValue(IconPathProperty, value); }
+    }
+    /// <summary>
+    /// 图标大小，默认 20
+    /// </summary>
+    public double IconSize {
+        get { return (double)GetValue(IconSizeProperty); }
+        set { SetValue(IconSizeProperty, value); }
+    }
+    /// <summary>
+    /// 是否自动获取 Icon，默认 true
+    /// </summary>
+    public bool AutoIcon {
+        get { return (bool)GetValue(AutoIconProperty); }
+        set { SetValue(AutoIconProperty, value); }
+    }
+    private static readonly IValueConverter FileIconConverter = new FileIconConverter();
+
+    public FileNameIconWidget() {
+        DependencyPropertyDescriptor.FromProperty(AutoIconProperty, typeof(FileNameIconWidget))
+            .AddValueChanged(this, AutoIconPropertyChangedHandler);
+        InitializeComponent();
+    }
+
+    /// <summary>
+    /// AutoIconProperty 改变
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void AutoIconPropertyChangedHandler(object? sender, EventArgs e) {
+        // 自动获取
+        if (AutoIcon) {
+            var binding = new Binding(nameof(FileName)) {
+                Converter = FileIconConverter
+            };
+            FileIconViewbox.SetBinding(SvgViewbox.SourceProperty, binding);
+            return;
+        }
+        FileIconViewbox.SetBinding(SvgViewbox.SourceProperty, new Binding(nameof(IconPath)));
+    }
+}
