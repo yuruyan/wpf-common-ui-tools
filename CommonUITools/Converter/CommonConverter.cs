9a3c7b0c3d03821e243d28542d026b8948ea98aa
Luxiu123 <klt5466@163.com>
2022-07-03 09:54:50 +0800

    feat: 添加 FileIconConverter

--- CommonUITools/Converter/CommonConverter.cs
+++ CommonUITools/Converter/CommonConverter.cs
@@ -550,3 +550,16 @@ public class BoolToStringConverter : IValueConverter {
         throw new NotImplementedException();
     }
 }
+
+/// <summary>
+/// 文件图标 Converter
+/// </summary>
+public class FileIconConverter : IValueConverter {
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+        return FileIconUtils.GetIcon(value.ToString() ?? string.Empty);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+        throw new NotImplementedException();
+    }
+}
