ebb445ce675d07cf331fdedf1da896ea774fc850
Luxiu123 <klt5466@163.com>
2022-07-06 21:37:49 +0800

    perf: 优化防抖函数

--- CommonUITools/Utils/DebounceUtils.cs
+++ CommonUITools/Utils/DebounceUtils.cs
@@ -2,6 +2,45 @@
 
 public class DebounceUtils {
     private static readonly IDictionary<Delegate, State> DebounceStateDict = new Dictionary<Delegate, State>();
+    private static readonly IDictionary<object, State2> DebounceState2Dict = new Dictionary<object, State2>();
+
+    /// <summary>
+    /// 防抖
+    /// </summary>
+    /// <param name="identifier"></param>
+    /// <param name="callback"></param>
+    /// <param name="callRegular">是否每隔 interval 时间调用一次</param>
+    /// <param name="interval"></param>
+    public static void Debounce(object identifier, Action callback, bool callRegular = false, int interval = 500) {
+        if (DebounceState2Dict.ContainsKey(identifier)) {
+            var value = DebounceState2Dict[identifier];
+            value.IsAccessed = true;
+            value.LastAccessTime = DateTime.Now;
+            return;
+        }
+        // 初始化
+        var state = new State2();
+        state.Timer.Interval = interval;
+        state.Timer.Elapsed += (o, e) => {
+            // 没有访问
+            if (!state.IsAccessed) {
+                return;
+            }
+            if (callRegular) {
+                DebounceState2Dict[identifier].IsAccessed = false;
+                callback();
+                return;
+            }
+            // 超过了 interval
+            if (state.LastAccessTime.AddMilliseconds(interval) <= DateTime.Now) {
+                DebounceState2Dict[identifier].IsAccessed = false;
+                DebounceState2Dict[identifier].LastAccessTime = DateTime.Now;
+                callback();
+            }
+        };
+        state.Timer.Start();
+        DebounceState2Dict[identifier] = state;
+    }
 
     /// <summary>
     /// 防抖函数
@@ -39,6 +78,12 @@ public class DebounceUtils {
             timer.Start();
         };
         return debounceFunc;
+    }
+
+    private record State2 {
+        public System.Timers.Timer Timer { get; set; } = new();
+        public DateTime LastAccessTime { get; set; } = DateTime.Now;
+        public bool IsAccessed { get; set; } = false;
     }
 
     private record State {
