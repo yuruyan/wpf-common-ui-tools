c187b8b8d947162a1fc904051c5e14113e79bb57
Luxiu123 <klt5466@163.com>
2022-08-13 17:13:34 +0800

    feat: 添加全局 WarningDialog

--- CommonUITools/Dialog/WarningDialog.xaml.cs
+++ CommonUITools/Dialog/WarningDialog.xaml.cs
@@ -1,8 +1,12 @@
-﻿namespace CommonUITools.View {
-    public partial class WarningDialog : BaseDialog {
+﻿namespace CommonUITools.View;
 
-        public WarningDialog(string title = "警告", string detailText = "") : base(title, detailText) {
-            InitializeComponent();
-        }
+public partial class WarningDialog : BaseDialog {
+    public WarningDialog(string title = "警告", string detailText = "") : base(title, detailText) {
+        InitializeComponent();
     }
+
+    /// <summary>
+    /// 全局 WarningDialog
+    /// </summary>
+    public static readonly WarningDialog Shared = new();
 }
