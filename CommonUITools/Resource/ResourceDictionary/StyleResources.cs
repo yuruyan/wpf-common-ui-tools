d294d5ac932adfdd9cc5faf5b472a2797c8e8bed
Luxiu123 <klt5466@163.com>
2022-08-16 09:12:55 +0800

    feat: 添加 OpenFileTextBlockStyle

---
+++ CommonUITools/Resource/ResourceDictionary/StyleResources.cs
@@ -0,0 +1,13 @@
+﻿using CommonUITools.Utils;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace CommonUITools.Resource.ResourceDictionary;
+
+public partial class StyleResources {
+    private void OpenFileTextBlockMouseUpHandler(object sender, MouseButtonEventArgs e) {
+        if (sender is TextBlock textBlock) {
+            UIUtils.OpenFileInDirectoryAsync(textBlock.Text ?? string.Empty);
+        }
+    }
+}
