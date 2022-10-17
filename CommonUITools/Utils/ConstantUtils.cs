6fd9b7350686f9803737865808ad5f755a9825ae
Luxiu123 <klt5466@163.com>
2022-10-17 14:43:41 +0800

    mod: 增加 ConstantUtils 属性

--- CommonUITools/Utils/ConstantUtils.cs
+++ CommonUITools/Utils/ConstantUtils.cs
@@ -1,12 +1,28 @@
 ﻿namespace CommonUITools.Utils;
 
 public static class ConstantUtils {
-    public const long OneMbSize = 1024 * 1024; // 1 MB 大小，以 byte 为单位
-    public const long OneGbSize = 1024 * 1024 * 1024; // 1 GB 大小，以 byte 为单位
-    public const int HalfHourSecond = 1800; // 半小时的秒数
-    public const int HalfHourMillisecond = 1800000; // 半小时的毫秒数
-    public const int OneHourSecond = 3600; // 1小时的秒数
-    public const int OneHourMillisecond = 3600000; // 1小时的毫秒数
-    public const int OneDaySecond = 86400; // 1天的秒数
-    public const int OneDayMillisecond = 86400000; // 1天的毫秒数
+    // 1 MB 大小，以 byte 为单位
+    public const long OneMbSize = 1024 * 1024;
+    // 1 GB 大小，以 byte 为单位
+    public const long OneGbSize = 1024 * 1024 * 1024;
+    // 半分钟的秒数
+    public const int HalfMinuteSecond = 30;
+    // 半分钟的毫秒数
+    public const int HalfMinuteMillisecond = HalfMinuteSecond * 1000;
+    // 1分钟的秒数
+    public const int OneMinuteSecond = HalfMinuteSecond << 1;
+    // 1分钟的毫秒数
+    public const int OneMinuteMillisecond = OneMinuteSecond * 1000;
+    // 半小时的秒数
+    public const int HalfHourSecond = 1800;
+    // 半小时的毫秒数
+    public const int HalfHourMillisecond = HalfHourSecond * 1000;
+    // 1小时的秒数
+    public const int OneHourSecond = HalfHourSecond << 1;
+    // 1小时的毫秒数
+    public const int OneHourMillisecond = OneHourSecond * 1000;
+    // 1天的秒数
+    public const int OneDaySecond = OneHourSecond * 24;
+    // 1天的毫秒数
+    public const int OneDayMillisecond = OneDaySecond * 1000;
 }
