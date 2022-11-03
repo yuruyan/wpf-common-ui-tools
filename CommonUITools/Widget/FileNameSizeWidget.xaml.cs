430df6a5bba1ca55e8216b8d336027f9ddfc53e4
Luxiu123 <klt5466@163.com>
2022-11-03 23:22:47 +0800

    feat: 添加 FileNameSizeWidget 组件

---
+++ CommonUITools/Widget/FileNameSizeWidget.xaml.cs
@@ -0,0 +1,20 @@
+﻿using ModernWpf.Controls;
+using System.Windows;
+
+namespace CommonUITools.Widget;
+
+public partial class FileNameSizeWidget : SimpleStackPanel {
+    public static readonly DependencyProperty FileNameProperty = DependencyProperty.Register("FileName", typeof(string), typeof(FileNameSizeWidget), new PropertyMetadata(string.Empty));
+
+    /// <summary>
+    /// 文件名
+    /// </summary>
+    public string FileName {
+        get { return (string)GetValue(FileNameProperty); }
+        set { SetValue(FileNameProperty, value); }
+    }
+
+    public FileNameSizeWidget() {
+        InitializeComponent();
+    }
+}
