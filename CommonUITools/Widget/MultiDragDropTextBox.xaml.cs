d18f76e11ee1f6422ef762a0e83b50efc4b1548c
Luxiu123 <klt5466@163.com>
2022-11-18 14:32:05 +0800

    feat: 添加 MultiDragDropTextBox 组件

---
+++ CommonUITools/Widget/MultiDragDropTextBox.xaml.cs
@@ -0,0 +1,168 @@
+﻿using CommonUITools.Utils;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CommonUITools.Widget;
+
+public partial class MultiDragDropTextBox : UserControl {
+    public static readonly DependencyProperty InputTextProperty = DependencyProperty.Register("InputText", typeof(string), typeof(MultiDragDropTextBox), new FrameworkPropertyMetadata(string.Empty, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+    public static readonly DependencyProperty TextBoxStyleProperty = DependencyProperty.Register("TextBoxStyle", typeof(Style), typeof(MultiDragDropTextBox), new PropertyMetadata());
+    public static readonly DependencyProperty HasFileProperty = DependencyProperty.Register("HasFile", typeof(bool), typeof(MultiDragDropTextBox), new PropertyMetadata(false));
+    private static readonly DependencyProperty FileNameListProperty = DependencyProperty.Register("FileNameList", typeof(ObservableCollection<string>), typeof(MultiDragDropTextBox), new PropertyMetadata());
+    public static readonly DependencyProperty FirstFileNameProperty = DependencyProperty.Register("FirstFileName", typeof(string), typeof(MultiDragDropTextBox), new PropertyMetadata(string.Empty));
+
+    /// <summary>
+    /// DragDropEvent，参数为 本次拖拽的 FileNames
+    /// </summary>
+    public event EventHandler<IList<string>>? DragDropEvent;
+
+    /// <summary>
+    /// 输入文本，默认双向绑定
+    /// </summary>
+    public string InputText {
+        get { return (string)GetValue(InputTextProperty); }
+        set { SetValue(InputTextProperty, value); }
+    }
+    /// <summary>
+    /// 文本框样式，默认 MultilineTextBoxStyle
+    /// </summary>
+    public Style TextBoxStyle {
+        get { return (Style)GetValue(TextBoxStyleProperty); }
+        set { SetValue(TextBoxStyleProperty, value); }
+    }
+    /// <summary>
+    /// 是否有文件
+    /// </summary>
+    public bool HasFile {
+        get { return (bool)GetValue(HasFileProperty); }
+        private set { SetValue(HasFileProperty, value); }
+    }
+    /// <summary>
+    /// 文件名列表
+    /// </summary>
+    private ObservableCollection<string> FileNameList {
+        get { return (ObservableCollection<string>)GetValue(FileNameListProperty); }
+        set { SetValue(FileNameListProperty, value); }
+    }
+    /// <summary>
+    /// 第一个文件名
+    /// </summary>
+    public string FirstFileName {
+        get { return (string)GetValue(FirstFileNameProperty); }
+        set { SetValue(FirstFileNameProperty, value); }
+    }
+    /// <summary>
+    /// 文件名集合
+    /// </summary>
+    private readonly ISet<string> FileNameSet = new HashSet<string>();
+    /// <summary>
+    /// 文件数据
+    /// </summary>
+    public object? FileData { get; private set; }
+    /// <summary>
+    /// 获取完整文件名列表
+    /// </summary>
+    public IList<string> FileNames => FileNameList.ToList();
+
+    public MultiDragDropTextBox() {
+        FileNameList = new();
+        // 更新 FirstFileName
+        FileNameList.CollectionChanged += (_, _) => {
+            if (FileNameList.Count > 0) {
+                FirstFileName = FileNameList[0];
+            }
+        };
+        DependencyPropertyDescriptor
+            .FromProperty(HasFileProperty, this.GetType())
+            .AddValueChanged(this, (_, _) => {
+                if (!HasFile) {
+                    FileData = null;
+                    FileNameSet.Clear();
+                    FileNameList.Clear();
+                }
+            });
+        InitializeComponent();
+        // 设置 TextBox 默认 Style
+        UIUtils.SetLoadedOnceEventHandler(this, (_, _) => {
+            if (TryFindResource("MultilineTextBoxStyle") is Style style) {
+                TextBoxStyle ??= style;
+            }
+        });
+    }
+
+    /// <summary>
+    /// Drop 事件
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void PreviewDropHandler(object sender, DragEventArgs e) {
+        FileData = e.Data.GetData(DataFormats.FileDrop);
+        if (FileData != null && FileData is ICollection<string> list && list.Count > 0) {
+            HasFile = true;
+            // 重复则不添加
+            list.ForEach(f => {
+                if (!FileNameSet.Contains(f)) {
+                    FileNameSet.Add(f);
+                    FileNameList.Add(f);
+                }
+            });
+            DragDropEvent?.Invoke(sender, list.ToList());
+        }
+    }
+
+    /// <summary>
+    /// 清空输入
+    /// </summary>
+    public void Clear() {
+        HasFile = false;
+        InputText = string.Empty;
+        FileData = null;
+        FileNameSet.Clear();
+        FileNameList.Clear();
+    }
+
+    /// <summary>
+    /// 打开文件点击
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void OpenFileClickHandler(object sender, RoutedEventArgs e) {
+        e.Handled = true;
+        if (sender is FrameworkElement element && element.DataContext is string path) {
+            UIUtils.OpenFileWithAsync(path);
+        }
+    }
+
+    /// <summary>
+    /// 打开文件夹
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void OpenDirectoryClickHandler(object sender, RoutedEventArgs e) {
+        e.Handled = true;
+        if (sender is FrameworkElement element && element.DataContext is string path) {
+            UIUtils.OpenFileInExplorerAsync(path);
+        }
+    }
+
+    /// <summary>
+    /// 移除文件
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void DeleteClickHandler(object sender, RoutedEventArgs e) {
+        e.Handled = true;
+        if (FileNameListBox.SelectedItems.Count > 0) {
+            var files = FileNameListBox.SelectedItems.Cast<string>().ToList();
+            for (int i = 0; i < files.Count; i++) {
+                var f = files[i];
+                FileNameList.Remove(f);
+                FileNameSet.Remove(f);
+            }
+            // 更新
+            HasFile = FileNameList.Count > 0;
+        }
+    }
+}
