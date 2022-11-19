4fca00ea7f833fba807c77f49cd710c69d442000
Luxiu123 <klt5466@163.com>
2022-11-19 23:36:31 +0800

    mod: 增加 FileProcessStatus FileSize 属性

--- CommonUITools/Model/FileProcessStatue.cs
+++ CommonUITools/Model/FileProcessStatue.cs
@@ -36,6 +36,7 @@ public class FileProcessStatus : DependencyObject {
     public static readonly DependencyProperty FileNameProperty = DependencyProperty.Register("FileName", typeof(string), typeof(FileProcessStatus), new PropertyMetadata(string.Empty));
     public static readonly DependencyProperty ProcessProperty = DependencyProperty.Register("Process", typeof(double), typeof(FileProcessStatus), new PropertyMetadata(0.0));
     public static readonly DependencyProperty StatusProperty = DependencyProperty.Register("Status", typeof(ProcessResult), typeof(FileProcessStatus), new PropertyMetadata(ProcessResult.NotStarted));
+    public static readonly DependencyProperty FileSizeProperty = DependencyProperty.Register("FileSize", typeof(long), typeof(FileProcessStatus), new PropertyMetadata(0L));
 
     /// <summary>
     /// 文件名
@@ -43,6 +44,13 @@ public class FileProcessStatus : DependencyObject {
     public string FileName {
         get { return (string)GetValue(FileNameProperty); }
         set { SetValue(FileNameProperty, value); }
+    }
+    /// <summary>
+    /// 文件大小
+    /// </summary>
+    public long FileSize {
+        get { return (long)GetValue(FileSizeProperty); }
+        set { SetValue(FileSizeProperty, value); }
     }
     /// <summary>
     /// 进度，[0 - 1]
@@ -57,5 +65,9 @@ public class FileProcessStatus : DependencyObject {
     public ProcessResult Status {
         get { return (ProcessResult)GetValue(StatusProperty); }
         set { SetValue(StatusProperty, value); }
+    }
+
+    public override string ToString() {
+        return $"{{{nameof(FileName)}={FileName}, {nameof(FileSize)}={FileSize.ToString()}, {nameof(Process)}={Process.ToString()}, {nameof(Status)}={Status.ToString()}, {nameof(DependencyObjectType)}={DependencyObjectType}, {nameof(IsSealed)}={IsSealed.ToString()}, {nameof(Dispatcher)}={Dispatcher}}}";
     }
 }
