bdb7b5ecd24ed4c622e33449f5608acf5b4d70fb
Luxiu123 <klt5466@163.com>
2022-11-24 12:20:07 +0800

    mod: 添加 Attributes、Exceptions 类

---
+++ CommonUITools/Model/Attributes.cs
@@ -0,0 +1,11 @@
+﻿namespace CommonUITools.Model;
+
+/// <summary>
+/// 表示不抛出异常
+/// </summary>
+[AttributeUsage(
+    AttributeTargets.Method | AttributeTargets.Constructor | AttributeTargets.Property,
+    Inherited = false,
+    AllowMultiple = true
+)]
+public sealed class NoExceptionAttribute : Attribute { }
---
+++ CommonUITools/Model/Exceptions.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.Serialization;
+
+namespace CommonUITools.Model;
+
+/// <summary>
+/// 解析错误
+/// </summary>
+public class ParseException : Exception {
+    public ParseException() { }
+
+    public ParseException(string? message) : base(message) { }
+
+    public ParseException(string? message, Exception? innerException) : base(message, innerException) { }
+
+    protected ParseException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+}
+
+/// <summary>
+/// 加载错误
+/// </summary>
+public class LoadException : Exception {
+    public LoadException() { }
+
+    public LoadException(string? message) : base(message) { }
+
+    public LoadException(string? message, Exception? innerException) : base(message, innerException) { }
+
+    protected LoadException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+}
