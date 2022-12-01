03bb8b9ec8810b1f2a0778c080823ca0e439a4a7
Luxiu123 <klt5466@163.com>
2022-12-01 11:51:38 +0800

    refactor: 重构 BaseDialog

--- CommonUITools/Dialog/BaseDialog.cs
+++ CommonUITools/Dialog/BaseDialog.cs
@@ -1,82 +1,75 @@
-﻿using ModernWpf.Controls;
+﻿using CommonUITools.Utils;
+using ModernWpf.Controls;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 
-namespace CommonUITools.View {
-    public class BaseDialog : ContentDialog {
-        /// <summary>
-        /// DetailText
-        /// </summary>
-        public string DetailText {
-            get { return (string)GetValue(DetailTextProperty); }
-            set { SetValue(DetailTextProperty, value); }
-        }
-        public static readonly DependencyProperty DetailTextProperty = DependencyProperty.Register("DetailText", typeof(string), typeof(BaseDialog), new PropertyMetadata(""));
+namespace CommonUITools.View;
 
-        public BaseDialog(
-            string title = "",
-            string detailText = "",
-            string okButtonText = "确定",
-            string cancelButtonText = "取消"
-        ) {
-            if (!string.IsNullOrEmpty(title)) {
-                Title = title;
-            }
-            if (!string.IsNullOrEmpty(detailText)) {
-                DetailText = detailText;
-            }
-            if (!string.IsNullOrEmpty(okButtonText)) {
-                PrimaryButtonText = okButtonText;
-            }
-            if (!string.IsNullOrEmpty(cancelButtonText)) {
-                CloseButtonText = cancelButtonText;
-            }
-            init();
-            // 设置 ScaleAnimation
-            Opened += (o, e) => {
-                Utils.ScaleAnimationHelper.SetIsEnabled((DependencyObject)this.Content, true);
-                Utils.ScaleAnimationHelper.SetScaleOption((DependencyObject)this.Content, Utils.ScaleAnimationHelper.ScaleOption.Center);
-            };
-        }
-
-        public BaseDialog() : this("") {
-        }
-
-        /// <summary>
-        /// 初始化
-        /// </summary>
-        private void init() {
-            DataContext = this;
-
-            DefaultButton = ContentDialogButton.Primary;
-            #region 设置 TitleTemplate
-            DataTemplate dataTemplate = new DataTemplate();
-            #region SimpleStackPanel
-            FrameworkElementFactory simpleStackPanel = new FrameworkElementFactory(typeof(SimpleStackPanel));
-            simpleStackPanel.SetValue(SimpleStackPanel.OrientationProperty, Orientation.Horizontal);
-            simpleStackPanel.SetValue(SimpleStackPanel.SpacingProperty, 8.0);
-            #region TextBlock
-            FrameworkElementFactory textblock = new FrameworkElementFactory(typeof(TextBlock));
-            textblock.SetValue(TextBlock.StyleProperty, Application.Current.Resources["ContentDialogTitleStyle"]);
-            textblock.SetBinding(TextBlock.TextProperty, new Binding("Title") {
-                RelativeSource = new RelativeSource(RelativeSourceMode.FindAncestor, typeof(BaseDialog), 1)
-            });
-            #endregion
-            simpleStackPanel.AppendChild(textblock);
-            #endregion
-            dataTemplate.VisualTree = simpleStackPanel;
-            TitleTemplate = dataTemplate;
-            #endregion
-        }
+public class BaseDialog : ContentDialog {
+    public static readonly DependencyProperty DetailTextProperty = DependencyProperty.Register("DetailText", typeof(string), typeof(BaseDialog), new PropertyMetadata(""));
+    /// <summary>
+    /// DetailText
+    /// </summary>
+    public string DetailText {
+        get { return (string)GetValue(DetailTextProperty); }
+        set { SetValue(DetailTextProperty, value); }
     }
 
-    public class DialogResult<T> {
-        public ContentDialogResult Result { get; set; }
-        public T? Data { get; set; } = default;
-
-        public override string ToString() {
-            return $"{nameof(Result)}: {Result}; {nameof(Data)}: {Data}";
+    public BaseDialog(
+        string title = "",
+        string detailText = "",
+        string okButtonText = "确定",
+        string cancelButtonText = "关闭"
+    ) {
+        Title = title;
+        DetailText = detailText;
+        PrimaryButtonText = okButtonText;
+        CloseButtonText = cancelButtonText;
+        Init();
+        if (TryFindResource("GlobalDefaultButtonStyle") is Style closeButtonStyle) {
+            CloseButtonStyle = closeButtonStyle;
         }
+        // 设置 ScaleAnimation
+        Opened += (dialog, _) => TaskUtils.EnsureCalledOnce((dialog, Application.Current), () => {
+            Utils.ScaleAnimationHelper.SetIsEnabled((DependencyObject)dialog.Content, true);
+            Utils.ScaleAnimationHelper.SetScaleOption((DependencyObject)dialog.Content, Utils.ScaleAnimationHelper.ScaleOption.Center);
+        });
+    }
+
+    public BaseDialog() : this("") { }
+
+    /// <summary>
+    /// 初始化
+    /// </summary>
+    private void Init() {
+        DataContext = this;
+        #region 设置 TitleTemplate
+        DataTemplate dataTemplate = new DataTemplate();
+        #region SimpleStackPanel
+        FrameworkElementFactory simpleStackPanel = new FrameworkElementFactory(typeof(SimpleStackPanel));
+        simpleStackPanel.SetValue(SimpleStackPanel.OrientationProperty, Orientation.Horizontal);
+        simpleStackPanel.SetValue(SimpleStackPanel.SpacingProperty, 8.0);
+        #region TextBlock
+        FrameworkElementFactory textblock = new FrameworkElementFactory(typeof(TextBlock));
+        textblock.SetValue(TextBlock.StyleProperty, Application.Current.Resources["ContentDialogTitleStyle"]);
+        textblock.SetBinding(TextBlock.TextProperty, new Binding("Title") {
+            RelativeSource = new RelativeSource(RelativeSourceMode.FindAncestor, typeof(BaseDialog), 1)
+        });
+        #endregion
+        simpleStackPanel.AppendChild(textblock);
+        #endregion
+        dataTemplate.VisualTree = simpleStackPanel;
+        TitleTemplate = dataTemplate;
+        #endregion
+    }
+}
+
+public class DialogResult<T> {
+    public ContentDialogResult Result { get; set; }
+    public T? Data { get; set; } = default;
+
+    public override string ToString() {
+        return $"{nameof(Result)}: {Result}; {nameof(Data)}: {Data}";
     }
 }
