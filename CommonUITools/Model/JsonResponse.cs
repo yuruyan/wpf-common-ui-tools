5f8363bed2c9c0ebe5d4d1970b9c1506366a8706
Luxiu123 <klt5466@163.com>
2022-12-01 14:36:16 +0800

    mod: 修改 JsonResponse

---
+++ CommonUITools/Model/JsonResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace CommonUITools.Model;
+
+public record class JsonResponse<T> {
+    public int Code { get; set; }
+    public string Message { get; set; } = string.Empty;
+    public T Data { get; set; } = default!;
+}
+
+public record class JsonResponse {
+    public int Code { get; set; }
+    public string Message { get; set; } = string.Empty;
+}
