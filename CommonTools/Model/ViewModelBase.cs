e7d92ed211ed1260fa732fc4816d06df335556d1
Luxiu123 <klt5466@163.com>
2023-02-08 21:24:41 +0800

    add BaseViewModel

---
+++ CommonTools/Model/ViewModelBase.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CommonTools.Model;
+
+public abstract class BaseViewModel : INotifyPropertyChanged {
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected void OnPropertyChanged([CallerMemberName] string propertyName = "") {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
