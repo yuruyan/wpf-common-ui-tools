00935a3966b21646930f2dfb91efeda3db50ed0f
Luxiu123 <klt5466@163.com>
2023-03-02 19:31:01 +0800

    Add GlobalNumberBoxStyle

---
+++ CommonUITools/Resource/ResourceDictionary/StyleResources.cs
@@ -0,0 +1,18 @@
+﻿using ModernWpf.Controls;
+
+namespace CommonUITools.Resource.ResourceDictionary;
+
+public partial class StyleResources {
+    /// <summary>
+    /// 转换浮点数为整数
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void IntegerNumberBoxLostFocusHandler(object sender, RoutedEventArgs e) {
+        e.Handled = true;
+        // 浮点数转整数
+        if (sender is NumberBox numberBox) {
+            TaskUtils.Try(() => numberBox.Value = (long)numberBox.Value);
+        }
+    }
+}
