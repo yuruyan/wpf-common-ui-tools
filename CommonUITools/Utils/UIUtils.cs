d8a71d7b64d7e4558488f4457f77a9d022c825f1
Luxiu123 <klt5466@163.com>
2023-03-10 11:27:22 +0800

    Add EnableContentDialogAutoResize

--- CommonUITools/Utils/UIUtils.cs
+++ CommonUITools/Utils/UIUtils.cs
@@ -1,4 +1,5 @@
 ﻿using CommonUITools.View;
+using ModernWpf.Controls;
 using System.Diagnostics;
 using System.Windows.Documents;
 using System.Windows.Media.Imaging;
@@ -70,6 +71,9 @@ public class CommonAdorner : Adorner {
     }
 }
 
+/// <summary>
+/// UI 工具类
+/// </summary>
 public static class UIUtils {
     private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
 
@@ -582,5 +586,58 @@ public static class UIUtils {
             return contentData;
         }
         return default;
+    }
+}
+
+/// <summary>
+/// Extention for UIUtils
+/// </summary>
+public static class UIUtilsExtension {
+    /// <summary>
+    /// ContentDialogResizeRatio
+    /// </summary>
+    public const double ContentDialogResizeRatio = 2;
+
+    /// <summary>
+    /// 启用 ContentDialog 自动调整大小
+    /// </summary>
+    /// <param name="dialog"></param>
+    /// <param name="minWidth"></param>
+    /// <param name="maxWidth"></param>
+    /// <param name="resizeRatio">NewWidth = Window.ActualWidth / <paramref name="resizeRatio"/></param>
+    public static void EnableContentDialogAutoResize(
+        this ContentDialog dialog,
+        double minWidth,
+        double maxWidth = double.MaxValue,
+        double resizeRatio = ContentDialogResizeRatio
+    ) {
+        UIUtils.SetLoadedOnceEventHandler(dialog, (_, _) => {
+            var window = Window.GetWindow(dialog);
+            var dialogDebounceId = new object();
+            // Update now
+            UpdateDialogWidth(dialog, window.ActualWidth / resizeRatio, minWidth, maxWidth);
+            window.SizeChanged += (_, arg) => DebounceUtils.Debounce(dialogDebounceId, () => {
+                // Reduce update frequency
+                dialog.Dispatcher.Invoke(() => {
+                    UpdateDialogWidth(dialog, arg.NewSize.Width / resizeRatio, minWidth, maxWidth);
+                });
+            }, true, 100);
+        });
+        // Update DialogContent Width
+        static void UpdateDialogWidth(
+            ContentDialog dialog,
+            double newWidth,
+            double minWidth,
+            double maxWidth
+        ) {
+            if (newWidth < minWidth) {
+                newWidth = minWidth;
+            } else if (newWidth > maxWidth) {
+                newWidth = maxWidth;
+            }
+            if (dialog.Content is FrameworkElement element) {
+                element.Width = newWidth;
+            }
+        }
     }
 }
