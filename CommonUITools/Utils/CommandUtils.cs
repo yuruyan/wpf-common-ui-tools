30cbaa38ed09efb9578bdbd8cdf4970c1fd1d8d8
Luxiu123 <klt5466@163.com>
2023-03-12 17:46:25 +0800

    Add CommandUtils

---
+++ CommonUITools/Utils/CommandUtils.cs
@@ -0,0 +1,13 @@
+﻿namespace CommonUITools.Utils;
+
+public static class CommandUtils {
+    /// <summary>
+    /// CanExecute = true
+    /// </summary>
+    public static readonly CanExecuteRoutedEventHandler CanExecuteHandler = CanExecute;
+
+    public static void CanExecute(object sender, CanExecuteRoutedEventArgs e) {
+        e.CanExecute = true;
+        e.Handled = true;
+    }
+}
