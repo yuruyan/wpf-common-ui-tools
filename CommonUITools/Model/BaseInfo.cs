a4b8af67b2975c09db63fced9a853173b9a93a06
Luxiu123 <klt5466@163.com>
2023-03-14 19:12:19 +0800

    Add BaseInfo

---
+++ CommonUITools/Model/BaseInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace CommonUITools.Model;
+
+public class BaseInfo {
+    public long Id { get; set; }
+}
+
+public class BaseInfoDO : DependencyObject {
+    public static readonly DependencyProperty IdProperty = DependencyProperty.Register("Id", typeof(long), typeof(BaseInfoDO), new PropertyMetadata(0L));
+
+    public long Id {
+        get { return (long)GetValue(IdProperty); }
+        set { SetValue(IdProperty, value); }
+    }
+}
