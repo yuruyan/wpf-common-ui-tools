1e66427fd219594f635c94622af3e25d15cb40c8
Luxiu123 <klt5466@163.com>
2023-03-18 17:30:16 +0800

    Add ToggleControl

---
+++ CommonUITools/Widget/ToggleControl.xaml.cs
@@ -0,0 +1,33 @@
+﻿namespace CommonUITools.Widget;
+
+public partial class ToggleControl : UserControl {
+    public static readonly DependencyProperty StateProperty = DependencyProperty.Register("State", typeof(bool), typeof(ToggleControl), new PropertyMetadata(true));
+    public static readonly DependencyProperty FirstControlProperty = DependencyProperty.Register("FirstControl", typeof(object), typeof(ToggleControl), new PropertyMetadata());
+    public static readonly DependencyProperty SecondControlProperty = DependencyProperty.Register("SecondControl", typeof(object), typeof(ToggleControl), new PropertyMetadata());
+
+    /// <summary>
+    /// 当 <see cref="State"/> 为 true 时显示
+    /// </summary>
+    public object FirstControl {
+        get { return (object)GetValue(FirstControlProperty); }
+        set { SetValue(FirstControlProperty, value); }
+    }
+    /// <summary>
+    /// 状态
+    /// </summary>
+    public bool State {
+        get { return (bool)GetValue(StateProperty); }
+        set { SetValue(StateProperty, value); }
+    }
+    /// <summary>
+    /// 当 <see cref="State"/> 为 false 时显示
+    /// </summary>
+    public object SecondControl {
+        get { return (object)GetValue(SecondControlProperty); }
+        set { SetValue(SecondControlProperty, value); }
+    }
+
+    public ToggleControl() {
+        InitializeComponent();
+    }
+}
