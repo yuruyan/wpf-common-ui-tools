96f5a0fa6622769413e8e50e07c1fea66e700dde
Luxiu123 <klt5466@163.com>
2023-03-22 12:05:59 +0800

    Adjust Enumeration files location

---
+++ CommonUITools/Model/Enumeration/ProcessResult.cs
@@ -0,0 +1,28 @@
+﻿namespace CommonUITools.Model;
+
+public enum ProcessResult {
+    /// <summary>
+    /// 未开始
+    /// </summary>
+    NotStarted,
+    /// <summary>
+    /// 处理中
+    /// </summary>
+    Processing,
+    /// <summary>
+    /// 暂停
+    /// </summary>
+    Paused,
+    /// <summary>
+    /// 终止
+    /// </summary>
+    Interrupted,
+    /// <summary>
+    /// 成功
+    /// </summary>
+    Successful,
+    /// <summary>
+    /// 失败
+    /// </summary>
+    Failed,
+}
--- CommonUITools/Model/FileProcessStatue.cs
+++ CommonUITools/Model/FileProcessStatue.cs
@@ -1,32 +1,5 @@
 ﻿namespace CommonUITools.Model;
 
-public enum ProcessResult {
-    /// <summary>
-    /// 未开始
-    /// </summary>
-    NotStarted,
-    /// <summary>
-    /// 处理中
-    /// </summary>
-    Processing,
-    /// <summary>
-    /// 暂停
-    /// </summary>
-    Paused,
-    /// <summary>
-    /// 终止
-    /// </summary>
-    Interrupted,
-    /// <summary>
-    /// 成功
-    /// </summary>
-    Successful,
-    /// <summary>
-    /// 失败
-    /// </summary>
-    Failed,
-}
-
 /// <summary>
 /// 文件进度状态
 /// </summary>
