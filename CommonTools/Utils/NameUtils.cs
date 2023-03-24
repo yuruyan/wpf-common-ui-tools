1f4c9828630a1575115b3577d2c0a4d38714fb16
Luxiu123 <klt5466@163.com>
2023-03-24 13:16:00 +0800

    Add NameUtils

---
+++ CommonTools/Utils/NameUtils.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace CommonTools.Utils;
+
+public static class NameUtils {
+    /// <summary>
+    /// 获取全名称表达式
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="_"></param>
+    /// <param name="fullName"></param>
+    /// <returns></returns>
+    public static string FullNameOf<T>(T _, [CallerArgumentExpression(nameof(_))] string fullName = "") {
+        return fullName;
+    }
+}
+
+public static class NameUtilsExtension {
+    /// <inheritdoc cref="NameUtils.FullNameOf{T}(T, string)"/>
+    public static string FullNameOf<T>(this T _, [CallerArgumentExpression(nameof(_))] string fullName = "") {
+        return fullName;
+    }
+}
