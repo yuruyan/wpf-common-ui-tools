8e6bc81b641c446ae44811bcc407d3345776771b
Luxiu123 <klt5466@163.com>
2023-03-28 19:56:30 +0800

    Add VisibilityIncludesConverterTests

---
+++ CommonUIToolsTests/Converter/VisibilityIncludesConverterTests.cs
@@ -0,0 +1,42 @@
+﻿using CommonUITools.Converter;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Globalization;
+using System.Windows;
+
+namespace CommonUIToolsTests.Converter;
+
+[TestClass()]
+public class VisibilityIncludesConverterTests {
+    [TestMethod()]
+    public void Convert() {
+        var converter = new VisibilityIncludesConverter();
+        Assert.AreEqual(
+            converter.Convert("0", typeof(Visibility), "[a b c]|Visible", CultureInfo.InvariantCulture),
+            Visibility.Visible
+        );
+        Assert.AreEqual(
+            converter.Convert("a", typeof(Visibility), "[a b c]|Visible", CultureInfo.InvariantCulture),
+            Visibility.Visible
+        );
+        Assert.AreEqual(
+            converter.Convert("a", typeof(Visibility), "[a b c]|Collapsed", CultureInfo.InvariantCulture),
+            Visibility.Collapsed
+        );
+        Assert.AreEqual(
+            converter.Convert("", typeof(Visibility), "[a b c]|Collapsed", CultureInfo.InvariantCulture),
+            Visibility.Visible
+        );
+        Assert.AreEqual(
+            converter.Convert(" ", typeof(Visibility), "[a b c]|Collapsed", CultureInfo.InvariantCulture),
+            Visibility.Visible
+        );
+        Assert.AreEqual(
+            converter.Convert(".......", typeof(Visibility), "[a b c]|Collapsed", CultureInfo.InvariantCulture),
+            Visibility.Visible
+        );
+        Assert.AreEqual(
+            converter.Convert(" ", typeof(Visibility), "[a    b      c]|Collapsed", CultureInfo.InvariantCulture),
+            Visibility.Visible
+        );
+    }
+}
