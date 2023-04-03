830130a4cf7197910359c0b3a646e24665d09ddb
Luxiu123 <klt5466@163.com>
2023-04-03 16:13:39 +0800

    Update JsonResponse model

--- CommonTools/Model/JsonResponse.cs
+++ CommonTools/Model/JsonResponse.cs
@@ -5,15 +5,15 @@ public record class JsonResponse<T> {
     public string Message { get; set; } = string.Empty;
     public T Data { get; set; } = default!;
 
-    public static JsonResponse<T> Success => new() { Code = 200, Message = nameof(Success) };
+    public static JsonResponse<T> Success(T data = default!) => new() { Data = data, Code = 200, Message = nameof(Success) };
 
-    public static JsonResponse<T> Error => new() { Code = 400, Message = nameof(Error) };
+    public static JsonResponse<T> Error(T data = default!) => new() { Data = data, Code = 400, Message = nameof(Error) };
 
-    public static JsonResponse<T> Forbidden => new() { Code = 403, Message = nameof(Forbidden) };
+    public static JsonResponse<T> Forbidden(T data = default!) => new() { Data = data, Code = 403, Message = nameof(Forbidden) };
 
-    public static JsonResponse<T> NotFound => new() { Code = 404, Message = nameof(NotFound) };
+    public static JsonResponse<T> NotFound(T data = default!) => new() { Data = data, Code = 404, Message = nameof(NotFound) };
 
-    public static JsonResponse<T> InternalError => new() { Code = 500, Message = nameof(InternalError) };
+    public static JsonResponse<T> InternalError(T data = default!) => new() { Data = data, Code = 500, Message = nameof(InternalError) };
 }
 
 public record class JsonResponse {
