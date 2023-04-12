2ccd41a63fc005ba5e730d6d3d0a1e4b53b42a5a
Luxiu123 <klt5466@163.com>
2023-04-12 09:53:48 +0800

    Fix ToggleControl

--- CommonUITools/Controls/ToggleControl.xaml.cs
+++ CommonUITools/Controls/ToggleControl.xaml.cs
@@ -1,23 +1,23 @@
 ﻿namespace CommonUITools.Controls;
 
-public class ToggleControl : System.Windows.Controls.Control {
+public class ToggleControl : Control {
     public static readonly DependencyProperty StateProperty = DependencyProperty.Register("State", typeof(bool), typeof(ToggleControl), new PropertyMetadata(true));
     public static readonly DependencyProperty FirstControlProperty = DependencyProperty.Register("FirstControl", typeof(object), typeof(ToggleControl), new PropertyMetadata());
     public static readonly DependencyProperty SecondControlProperty = DependencyProperty.Register("SecondControl", typeof(object), typeof(ToggleControl), new PropertyMetadata());
 
-    /// <summary>
-    /// 当 <see cref="State"/> 为 true 时显示
-    /// </summary>
-    public object FirstControl {
-        get { return (object)GetValue(FirstControlProperty); }
-        set { SetValue(FirstControlProperty, value); }
-    }
     /// <summary>
     /// 状态
     /// </summary>
     public bool State {
         get { return (bool)GetValue(StateProperty); }
         set { SetValue(StateProperty, value); }
+    }
+    /// <summary>
+    /// 当 <see cref="State"/> 为 true 时显示
+    /// </summary>
+    public object FirstControl {
+        get { return (object)GetValue(FirstControlProperty); }
+        set { SetValue(FirstControlProperty, value); }
     }
     /// <summary>
     /// 当 <see cref="State"/> 为 false 时显示
@@ -29,5 +29,22 @@ public class ToggleControl : System.Windows.Controls.Control {
 
     static ToggleControl() {
         DefaultStyleKeyProperty.OverrideMetadata(typeof(ToggleControl), new FrameworkPropertyMetadata(typeof(ToggleControl)));
+    }
+
+    public ToggleControl() {
+        this.SetLoadedOnceEventHandler((_, _) => {
+            if (Template.FindName("FirstControl", this) is ContentPresenter firstControl) {
+                firstControl.SetBinding(VisibilityProperty, new Binding(nameof(State)) {
+                    RelativeSource = new(RelativeSourceMode.TemplatedParent),
+                    Converter = (IValueConverter)FindResource("HideIfFalseConverter")
+                });
+            }
+            if (Template.FindName("SecondControl", this) is ContentPresenter secondControl) {
+                secondControl.SetBinding(VisibilityProperty, new Binding(nameof(State)) {
+                    RelativeSource = new(RelativeSourceMode.TemplatedParent),
+                    Converter = (IValueConverter)FindResource("HideIfTrueConverter")
+                });
+            }
+        });
     }
 }
