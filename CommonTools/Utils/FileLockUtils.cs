983a26ea6d6a8b5ba432f6a6e40030b18bcd7b20
Luxiu123 <klt5466@163.com>
2023-04-14 14:29:42 +0800

    Add FileLockUtils

---
+++ CommonTools/Utils/FileLockUtils.cs
@@ -0,0 +1,34 @@
+﻿namespace CommonTools.Utils;
+
+public static class FileLockUtils {
+    /// <summary>
+    /// 尝试锁定文件
+    /// </summary>
+    /// <param name="filepath"></param>
+    /// <returns></returns>
+    public static FileStream? TryLockFile(string filepath) {
+        return TaskUtils.Try(() => File.Open(
+            filepath,
+            FileMode.OpenOrCreate,
+            FileAccess.Read,
+            FileShare.None
+        ), null);
+    }
+
+    /// <summary>
+    /// 文件是否锁定
+    /// </summary>
+    /// <param name="filepath"></param>
+    /// <returns></returns>
+    public static bool IsFileLocked(string filepath) {
+        return TaskUtils.Try<bool?>(() => {
+            using var file = File.Open(
+                filepath,
+                FileMode.OpenOrCreate,
+                FileAccess.Read,
+                FileShare.None
+            );
+            return false;
+        }, null) ?? true;
+    }
+}
