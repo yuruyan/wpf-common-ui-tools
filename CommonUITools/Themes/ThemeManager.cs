8a1881b00c137c955a308fe236a1944e08622584
Luxiu123 <klt5466@163.com>
2023-04-18 16:12:24 +0800

    Update ThemeManager

--- CommonUITools/Themes/ThemeManager.cs
+++ CommonUITools/Themes/ThemeManager.cs
@@ -2,15 +2,17 @@
 
 public static class ThemeManager {
     private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+    private static readonly ResourceDictionary GenericResourceDictionary;
 
     private const string GenericSource1 = "/CommonUITools;component/Themes/Generic.xaml";
     private const string GenericSource2 = "pack://application:,,,/CommonUITools;component/Themes/Generic.xaml";
     private const string LightThemeSource = "/CommonUITools;component/Themes/LightThemeResources.xaml";
     private const string DarkThemeSource = "/CommonUITools;component/Themes/DarkThemeResources.xaml";
 
-    private static readonly ResourceDictionary GenericResourceDictionary;
+    public static ThemeMode CurrentTheme { get; private set; } = ThemeMode.Light;
 
     static ThemeManager() {
+        ModernWpf.ThemeManager.Current.ApplicationTheme = ModernWpf.ApplicationTheme.Light;
         var dictionary = Application.Current.Resources.MergedDictionaries;
         // 查找 GenericResourceDictionary
         var res = dictionary.FindResource(GenericSource1);
@@ -25,21 +27,29 @@ public static class ThemeManager {
     /// 切换为 LightTheme
     /// </summary>
     public static void SwitchToLightTheme() {
+        if (CurrentTheme == ThemeMode.Light) {
+            return;
+        }
         GenericResourceDictionary.MergedDictionaries.ReplaceResourceDictionary(
             DarkThemeSource,
             LightThemeSource
         );
         ModernWpf.ThemeManager.Current.ApplicationTheme = ModernWpf.ApplicationTheme.Light;
+        CurrentTheme = ThemeMode.Light;
     }
 
     /// <summary>
     /// 切换为 DarkTheme
     /// </summary>
     public static void SwitchToDarkTheme() {
+        if (CurrentTheme == ThemeMode.Dark) {
+            return;
+        }
         GenericResourceDictionary.MergedDictionaries.ReplaceResourceDictionary(
             LightThemeSource,
             DarkThemeSource
         );
         ModernWpf.ThemeManager.Current.ApplicationTheme = ModernWpf.ApplicationTheme.Dark;
+        CurrentTheme = ThemeMode.Dark;
     }
 }
