44bf983656a847d4b1f4733eb53e651e38ea581b
yuruyan <luxiu.klt@foxmail.com>
2024-03-25 11:45:14 +0800

    Add SharedLogging,ArgumentUtils

---
+++ CommonTools/Utils/ArgumentUtils.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace CommonTools.Utils;
+
+public static class ArgumentUtils {
+    /// <summary>
+    /// 检查参数是否有参数，没有则输出帮助信息并返回 false
+    /// </summary>
+    /// <param name="args"></param>
+    /// <param name="helpMessage">参数为 0 时的提示消息</param>
+    /// <returns></returns>
+    public static bool CheckArgs(string[] args, string helpMessage) {
+        if (args.Length == 0) {
+            Console.WriteLine(helpMessage);
+            return false;
+        }
+        return true;
+    }
+
+    /// <summary>
+    /// 获取 Configuration
+    /// </summary>
+    /// <param name="args"></param>
+    /// <returns></returns>
+    public static IConfiguration GetConfiguration(this string[] args) => new ConfigurationBuilder().AddCommandLine(args).Build();
+
+    /// <summary>
+    /// 是否包含 wait 参数
+    /// </summary>
+    /// <param name="args"></param>
+    /// <returns></returns>
+    public static bool ContainsWaitArgument(this string[] args) {
+        args = args.Select(arg => arg.ToLowerInvariant()).ToArray();
+        return args.Contains("--wait") || args.Contains("/wait");
+    }
+
+
+    /// <summary>
+    /// 检查文件是否存在
+    /// </summary>
+    /// <param name="filePath">文件路径</param>
+    /// <param name="argumentName">命令行参数名称</param>
+    /// <returns>存在返回 true</returns>
+    public static bool ValidateFileArgument(string? filePath, string argumentName) {
+        if (string.IsNullOrEmpty(filePath)) {
+            SharedLogging.Logger.LogError("Argument '{argumentName}' cannot be empty", argumentName);
+            return false;
+        }
+        if (!File.Exists(filePath)) {
+            SharedLogging.Logger.LogError("File \"{path}\" doesn't exist", filePath);
+            return false;
+        }
+        return true;
+    }
+
+    /// <summary>
+    /// 检查文件夹是否存在
+    /// </summary>
+    /// <param name="directory">文件夹路径</param>
+    /// <param name="argumentName">命令行参数名称</param>
+    /// <returns></returns>
+    public static bool ValidateDirectoryArgument(string? directory, string argumentName) {
+        if (string.IsNullOrEmpty(directory)) {
+            SharedLogging.Logger.LogError("Argument '{argumentName}' cannot be empty", argumentName);
+            return false;
+        }
+        if (!Directory.Exists(directory)) {
+            SharedLogging.Logger.LogError("Directory \"{path}\" doesn't exist", directory);
+            return false;
+        }
+        return true;
+    }
+
+    /// <summary>
+    /// 等待用户按键退出
+    /// </summary>
+    public static void WaitUserInputToExit() {
+        Process.Start(Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.System), "cmd.exe"),
+            "/c pause"
+        ).WaitForExit();
+    }
+}
---
+++ CommonTools/Utils/SharedLogging.cs
@@ -0,0 +1,36 @@
+﻿using Karambolo.Extensions.Logging.File;
+using Microsoft.Extensions.Logging;
+
+namespace CommonTools;
+
+public static class SharedLogging {
+    public static readonly ILogger FileLogger;
+    public static readonly ILogger Logger;
+    public static readonly ILoggerFactory FileLoggerFactory;
+    public static readonly ILoggerFactory LoggerFactory;
+
+    static SharedLogging() {
+        LoggerFactory = Microsoft.Extensions.Logging.LoggerFactory.Create(builder => builder.AddConsole());
+        FileLoggerFactory = Microsoft.Extensions.Logging.LoggerFactory.Create(
+            builder => builder.AddConsole().AddFile(context => {
+                context.RootPath = Path.GetDirectoryName(Environment.ProcessPath);
+                context.Files = [new LogFileOptions() {
+                    Path = "app.log",
+                    MinLevel = new Dictionary<string, LogLevel> {
+                        {"Default", LogLevel.Warning}
+                    }
+                }];
+            })
+        );
+        Logger = LoggerFactory.CreateLogger("Main");
+        FileLogger = FileLoggerFactory.CreateLogger("Main");
+    }
+
+    /// <summary>
+    /// Dispose
+    /// </summary>
+    public static void Dispose() {
+        LoggerFactory.Dispose();
+        FileLoggerFactory.Dispose();
+    }
+}
